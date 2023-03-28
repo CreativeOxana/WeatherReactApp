0c02c01a8742848b26aebe699b8128e2a0491f76
Oxana <110328307+CreativeOxana@users.noreply.github.com>
2023-03-28 17:16:01 +0200

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -27,7 +27,7 @@ export default function WeatherInfo(props) {
               </div>
             </div>
           </div>
-          <div className="col-12 d-flex justify-content-center align-items-center w-150">
+          <div className="col-12 d-flex justify-content-between align-items-center w-150">
               <div>Humidity: {props.data.humidity} %</div>
               <div>Wind: {props.data.windspeed} km/h</div>
           </div>
